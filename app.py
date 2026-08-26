39eaae5724446fee57dfbb0032e231dee827c194
harrymddl <harry_miles@hotmail.com>
2026-08-26 04:30:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 DATA_FILE = "darts_data.json"
-GITHUB_REPO = "harrymddl/DDLELOTRACKER"
+GITHUB_REPO = "harrymddl/ddlelotracker"
 
 
 # --- 1. DATA STORAGE & GITHUB AUTO-SYNC ---
